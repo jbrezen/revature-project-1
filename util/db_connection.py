8c5153938847d91a1f8c3ddc481dacbb8a6cd035
jbrezen <82970206+jbrezen@users.noreply.github.com>
2021-05-09 19:09:44 -0400

    Update db_connection.py

--- util/db_connection.py
+++ util/db_connection.py
@@ -5,11 +5,7 @@ from psycopg2._psycopg import OperationalError
 def create_connection():
     try:
         conn = psycopg2.connect(
-            database='postgres',
-            user='postgres',
-            password='SuperJ98!',
-            host='jbrezenpostgres.cjssqswshuti.us-east-2.rds.amazonaws.com',
-            port='5432'
+            # Redacted for GitHub
         )
         return conn
     except OperationalError as e:
